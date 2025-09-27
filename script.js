13f19e00f13eaa01de77b90ac64c9d453f49ed68
ShadowPrincee <114082125+ShadowPrincee@users.noreply.github.com>
2025-09-27 19:19:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simple Contact Form Alert
-const contactForm = document.getElementById("contactForm");
-contactForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Thank you! Your message has been sent.");
-  contactForm.reset();
-});
